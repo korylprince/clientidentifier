2d1776f4851af022c6c0dc2984856061d70f88dd
Kory Prince <korylprince@gmail.com>
2020-03-02 14:50:21 -0600

    converted to munkipkg

--- mkpkg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ver="1.2"
-./build.sh
-sudo cp build/clientidentifier /usr/local/bin/
-sudo chown root:wheel /usr/local/bin/clientidentifier
-sudo pkggen -i tk.unstac.clientidentifier -v "$ver" files out.pkg
-sudo chown administrator:staff out.pkg
-mv out.pkg "clientidentifier-$ver.pkg"
